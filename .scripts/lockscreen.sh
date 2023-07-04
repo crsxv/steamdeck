e1e18a4621c34e8a4a1fdfab33dfa3d0e11908cb
crsxv <crsxv@users.noreply.github.com>
2023-07-04 02:58:57 -0400

    Update lockscreen.sh

--- .scripts/lockscreen.sh
+++ .scripts/lockscreen.sh
@@ -4,6 +4,40 @@
 # "DisplayPort-0" = Monitor
 # "eDP" = Steam Deck Built-in screen
 
+# It enables strict error handling and behavior for the script
+set -euo pipefail
+
+# List of required packages
+required_packages=(
+    "konsole"
+    "rsync"
+    "zenity"
+)
+
+# Function to check if a package is installed
+is_package_installed() {
+    local package="$1"
+    pacman -Q "$package" &>/dev/null
+}
+
+# Check if any of the required packages are not installed
+missing_packages=()
+for package in "${required_packages[@]}"; do
+    if ! is_package_installed "$package"; then
+        missing_packages+=("$package")
+    fi
+done
+
+# If there are missing packages, install them and disable steamos-readonly temporarily
+if [ ${#missing_packages[@]} -gt 0 ]; then
+    echo "Installing missing packages: ${missing_packages[*]}"
+    sudo steamos-readonly disable
+    sudo pacman -Sy --noconfirm "${missing_packages[@]}"
+    sudo steamos-readonly enable
+else
+    echo "Packages already installed."
+fi
+
 display=$(xrandr --listmonitors | awk 'NR==2{print $4}')
 
 if [ "$display" = "DisplayPort-0" ]; then
